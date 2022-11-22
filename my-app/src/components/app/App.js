7d88426474ba26a8a65d61fa7afe2d0aa197fcc5
sjmugi <jeremyisaac525@gmail.com>
2022-11-22 11:49:37 +0000

    all component files created

---
+++ my-app/src/components/app/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return;(
+    <main>
+    <div>
+      <Header/>
+    
+    </div>
+    <div>
+      <Sidebar />
+    </div>
+    <div>
+      <Main-section />
+    </div>
+    </main>
+   
+  )
+}
+
+export default App;
+//inside header 
+// - navigation arrows
+// - title
+
+//inside Sidebar
+// - different days
+
+//main-section
+// - morning/afternoon buttons
+// - post section
+// - comments section
