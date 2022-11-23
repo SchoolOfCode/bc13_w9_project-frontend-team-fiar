427990a326b9605b49476f76592a1178caf57b38
sjmugi <jeremyisaac525@gmail.com>
2022-11-23 11:03:53 +0000

    input field and ubmit button made

--- my-app/src/components/main-section/index.js
+++ my-app/src/components/main-section/index.js
@@ -3,11 +3,12 @@ import "./index.css";
 //import MorningAfternoonButton from "../morning and afternoon buttons";
 //import CommentsSection from "../comment section";
 //import PostsSection from "../post section"
+import CommentSection from "../comment section";
 
 function MainSection() {
   return (
     <div>
-      
+      <CommentSection />
     </div>
   );
 }
